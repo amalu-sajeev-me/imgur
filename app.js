147da520fad99a355461a6fdee5a9f545caebf3d
Amalu Sajeev <amalu.sajeev.me@gmail.com>
2021-11-27 20:12:00 +0530

    signup with database

--- app.js
+++ app.js
@@ -1,29 +1,13 @@
 await import("dotenv").then((dotenv) => dotenv.config());
-import mongoose from "mongoose";
-import { join, resolve } from "path";
-import { default as express } from "express";
-import bodyParser from "body-parser";
 const { HOST, PORT } = process.env;
+import { default as express } from "express";
+import { routes } from './route.js'
 const app = express();
-app.set("view engine", "ejs");
-app.use(express.static("public"));
-app.set("views", join(resolve(), "/public"));
-const dblink = `mongodb+srv://dev:passmein@cluster0.gsr2u.mongodb.net/imgur?retryWrites=true&w=majority`;
-mongoose.connect(dblink).then(() => {
-  console.log("database connection succesfull");
-});
-app.use(bodyParser.urlencoded({ extended: true }));
-app.get("/", (req, res) => {
-  res.render("index");
-});
-app.post("/new", (req, res) => {
-  let { user } = req.body;
-  res.render("profile", {
-    name: user,
-  });
-});
-// server
+app.use(express.urlencoded({ extended: true }))
+  .use(express.static('public'))
+  .set('view engine', 'ejs')
+  .set('views', 'public')
+  .use(routes);
 app.listen(PORT, HOST, () => {
-  console.log("server started at", process.env.PORT);
-  console.log(resolve());
+  console.log(`server started at ${HOST}:${PORT}`);
 });
--- route.js
+++ route.js
@@ -1,2 +1,39 @@
-import { Router} from "express";
+import { Router } from "express";
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+const db_string = `mongodb+srv://dev:passmein@cluster0.gsr2u.mongodb.net/imgur?retryWrites=true&w=majority`;
+const userSchema = new mongoose.Schema({
+  user: String,
+  pass: String,
+});
+const User = mongoose.model("User", userSchema);
+
 const router = Router();
+router
+  .get("/", (request, response) => {
+    response.render("index", { err: false });
+  })
+  .post("/", async (request, response) => {
+    const { user, pass } = request.body;
+    const hash = await bcrypt.hash(pass, 12);
+    mongoose
+      .connect(db_string)
+      .then(() => {
+        User.findOne({}).then((data) => {
+          if (data.user === user) {
+            let err = "choose a different username";
+            response.render("index", { err });
+          } else {
+            let newUser = new User({ user, pass: hash });
+            newUser.save();
+            response.redirect("/profile");
+          }
+        });
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+    // response.render('index', { err: request.body.user });
+  });
+
+export { router as routes };
