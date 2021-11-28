33c6f2b0fb9fc0c56502f395328d3d7f329f27fa
Amalu Sajeev <amalu.sajeev.me@gmail.com>
2021-11-29 00:23:24 +0530

    test fetch 16

--- routes/profile.js
+++ routes/profile.js
@@ -3,10 +3,26 @@ import mongoose from "mongoose";
 const ObjectId = mongoose.Types.ObjectId;
 import { User } from "../route.js";
 // import session from 'express-session';
+import imgur from "imgur";
+const clientId = `e86892e30f94c31`;
+imgur.setClientId(clientId);
+imgur.setAPIUrl("https://api.imgur.com/3/upload");
+import fs from "fs";
+let rand = Math.random() * 1000;
+
 const db_string = `mongodb+srv://dev:passmein@cluster0.gsr2u.mongodb.net/imgur?retryWrites=true&w=majority`;
 const router = Router();
 router
   .get("/", (request, response) => {
+    // dummy
+    // fs.writeFile(rand, "hey dummy", "utf8", function (err) {
+    //   if (err) console.log(err.message);
+    // });
+    fs.writeFile(`${rand}.txt`, "Hello dummy content!", function (err) {
+      if (err) throw err;
+      console.log("Saved!");
+    });
+    // dummy end
     const { user_id } = request.session;
     if (!request.session.user_id) {
       response.redirect("/");
