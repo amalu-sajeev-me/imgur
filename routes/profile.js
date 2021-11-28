02b450bf78d9fa963bb570310148ba99ce5a470f
Amalu Sajeev <amalu.sajeev.me@gmail.com>
2021-11-28 17:30:11 +0530

    bug fix trial4

--- routes/profile.js
+++ routes/profile.js
@@ -1,29 +1,26 @@
-import { Router } from 'express';
-import mongoose from 'mongoose';
-const OjectId = mongoose.Types.ObjectId;
-import { User } from '../route.js';
+import { Router } from "express";
+import mongoose from "mongoose";
+const ObjectId = mongoose.Types.ObjectId;
+import { User } from "../route.js";
 // import session from 'express-session';
 const db_string = `mongodb+srv://dev:passmein@cluster0.gsr2u.mongodb.net/imgur?retryWrites=true&w=majority`;
 const router = Router();
-router.get('/', (request, response) => {
-  const { user_id } = request.session;
-  if (!request.session.user_id) {
-    response.redirect('/')
-  } else {
-    mongoose.connect(db_string).then(async () => {
-      const result = await User.findOne({ _id: new ObjectId(user_id) });
-      console.log(result, user_id);
-      response.render("profile", { id: user_id, name: result?.user });
-    });
-  }
-})
-  .get('/logout', (request, response) => {
+router
+  .get("/", (request, response) => {
+    const { user_id } = request.session;
+    if (!request.session.user_id) {
+      response.redirect("/");
+    } else {
+      mongoose.connect(db_string).then(async () => {
+        const result = await User.findOne({ _id: new ObjectId(user_id) });
+        console.log(result, user_id);
+        response.render("profile", { id: user_id, name: result?.user });
+      });
+    }
+  })
+  .get("/logout", (request, response) => {
     request.session.destroy();
-    response.redirect('/');
-})
-
-
-
-
+    response.redirect("/");
+  });
 
 export { router as profile };
